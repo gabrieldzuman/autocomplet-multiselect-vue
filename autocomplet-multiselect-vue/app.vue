97f2b93479dac2dcd58dc26b8ecef71b0031e3c1
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-07-29 01:23:00 -0300

    Add files via upload

---
+++ autocomplet-multiselect-vue/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <VueMultiselect
+      v-model="multiValue"
+      :options="source"
+      :multiple="true"
+      :close-on-select="true"
+      placeholder="Autocomplete Funciona!"
+      label="name"
+      track-by="name" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import VueMultiselect from "vue-multiselect";
+const multiValue = ref(null);
+const source = ref([
+  { name: "Gabriel!" },
+  { name: "Leonardo!" },
+  { name: "Dzuman!" },
+]);
+</script>
+
+<style src="vue-multiselect/dist/vue-multiselect.css"></style>
